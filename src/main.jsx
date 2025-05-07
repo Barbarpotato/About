d3d60593afd2a0cbf0ad2ecae2d6e070b944f25c
Barbarpotato <darmawanjr88@gmail.com>
2025-05-07 10:33:09 +0800

    add darwin

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import { Box, ChakraProvider } from '@chakra-ui/react'
+import { Darwin } from 'personal-shared-layout'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
@@ -7,6 +8,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ChakraProvider>
+    <Darwin />
     <App />
   </ChakraProvider>
 )
