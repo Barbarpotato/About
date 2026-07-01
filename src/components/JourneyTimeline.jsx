2af550b7c64541f61cdc6f6d19eddafb19b4adaa
Barbarpotato <darmawanjr88@gmail.com>
2026-07-02 07:50:58 +0800

    improve design

--- src/components/JourneyTimeline.jsx
+++ src/components/JourneyTimeline.jsx
@@ -168,7 +168,7 @@ function JourneyTimeline() {
                 <MotionBox
                     initial={{ opacity: 0, y: 20 }}
                     whileInView={{ opacity: 1, y: 0 }}
-                    viewport={{ once: true, amount: 0.15 }}
+                    viewport={{ once: true, amount: 0 }}
                     transition={{ duration: 0.6, delay: 0.15 }}
                     className="journey-chrono"
                 >
